bb82a9d3935787907aab6d4f91f4b296c6fee0fb
Filip <filip.bergander1996@gmail.com>
2026-01-27 10:07:40 +0100

    Async funktion importData som hämtar in extern url och skriver ut till table.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,38 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", async() => {
+    importData();
+    document.getElementById("search").addEventListener("input", filterData);
 });
+
+let allData = [];
+
+const code = [
+    {}
+]
+
+async function importData() {
+    try {
+        const response = await fetch(
+            "https://webbutveckling.miun.se/files/ramschema.json"
+        );
+        const info = await response.json();
+        console.table(info)
+        showData(info);
+    } catch (error) {
+        console.error("Felmeddelanden: ", error);
+    }
+}
+
+function showData(info) {
+    const tableEl = document.getElementById("main-table");
+    info.forEach(row => { //Loopa genom varje index
+        tableEl.innerHTML += `
+    <tr>
+        <td>${row.code}</td>
+        <td>${row.coursename}</td>
+        <td>${row.progression}</td>
+    </tr>
+        `
+    });
+}

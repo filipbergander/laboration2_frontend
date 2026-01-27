e0218554fabed2b55d15ee6ed53ef12737878a3d
Filip <filip.bergander1996@gmail.com>
2026-01-27 10:53:37 +0100

    Skapat sökfunktion.

--- src/js/main.js
+++ src/js/main.js
@@ -10,37 +10,36 @@ let allData = [];
 
 async function importData() {
     try {
-        const response = await fetch(
+        const response = await fetch( // Hämtar url
             "https://webbutveckling.miun.se/files/ramschema.json"
         );
-        const info = await response.json();
-        console.table(info)
+        const info = await response.json(); // Sparar datan i variabeln info
         showData(info);
         allData = info
     } catch (error) {
-        console.error("Felmeddelanden: ", error);
+        console.error("Felmeddelanden: ", error); // Felmeddelande vid error
     }
 }
 
 function showData(info) {
-    const tableEl = document.getElementById("table-body");
-    tableEl.innerHTML = ""; // Rensar listan 
+    const tableBodyEl = document.getElementById("table-body");
+    tableBodyEl.innerHTML = ""; // Rensar listan 
     info.forEach(row => { //Loopa genom varje index
-        tableEl.innerHTML += `
+        tableBodyEl.innerHTML += `
     <tr>
         <td>${row.code}</td>
         <td>${row.coursename}</td>
         <td>${row.progression}</td>
     </tr>
-        `
+        ` // Skapar en tabell där datan ska skrivas ut i
     });
 }
 
-function filterData() {
-    const searchPhrase = document.getElementById("search").value;
+function filterData() { // För att filtrera datan 
+    const searchPhrase = document.getElementById("search").value.toLowerCase(); // Hämtar in värde som skrivs i sökfältet
     const filteredData = allData.filter((row) =>
-        row.code.toLowerCase().includes(searchPhrase) ||
+        row.code.toLowerCase().includes(searchPhrase) || // Om något söks på kodnamn
         row.coursename.toLowerCase().includes(searchPhrase) ||
         row.progression.toLowerCase().includes(searchPhrase));
-    showData(filteredData)
+    showData(filteredData) // Kallar på funktionen igen men genom att filtrera
 }
